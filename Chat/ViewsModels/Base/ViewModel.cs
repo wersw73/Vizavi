3db9fdd022acdf9302c0995b4af78354dd1b2837
wersw73 <wersw73@gmail.com>
2023-02-13 14:33:45 +0400

    Binding Title

---
+++ Chat/ViewsModels/Base/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.ViewsModels.Base {
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string? PropertyName = null) {
+            PropertyChanged?.Invoke ( this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T> ( ref T field, T value, [CallerMemberName] string? PropertyName = null ) {
+            if ( Equals ( field, value ) ) {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged ( PropertyName );
+            return true;
+        }
+     
+       // ~ViewModel () {
+       //     Dispose ( false );
+       // }
+
+
+        public void Dispose () {
+            Dispose( true );
+        }
+        private bool _Disposed;
+
+        protected virtual void Dispose (bool Disposing) {
+            if ( _Disposed || _Disposed ) return;
+            _Disposed = true;
+        }
+          
+    }
+}
---
+++ Chat/ViewsModels/MainWindowViewModels.cs
@@ -0,0 +1,19 @@
+﻿using Chat.ViewsModels.Base;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.ViewsModels {
+    internal class MainWindowViewModels:ViewModel {
+
+        private string _Title = "Chat Vizavi";
+
+        public string Title {
+            get => _Title;
+            set => Set ( ref _Title,  value );
+        }
+    }
+}
